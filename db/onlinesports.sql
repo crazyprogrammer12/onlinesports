0a02cd861a2659b3f74b322ea29509478ee33e9f
Prem Bahadur Basnet <prem.basnet@gibl.com.np>
2025-01-15 07:17:04 +0545

    First code push of my webiste Version 1.0

---
+++ db/onlinesports.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 15, 2025 at 02:17 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `onlinesports`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedbacks`
+--
+-- Error reading structure for table onlinesports.feedbacks: #1932 - Table &#039;onlinesports.feedbacks&#039; doesn&#039;t exist in engine
+-- Error reading data for table onlinesports.feedbacks: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `onlinesports`.`feedbacks`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+-- Error reading structure for table onlinesports.products: #1932 - Table &#039;onlinesports.products&#039; doesn&#039;t exist in engine
+-- Error reading data for table onlinesports.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `onlinesports`.`products`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table onlinesports.users: #1932 - Table &#039;onlinesports.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table onlinesports.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `onlinesports`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
